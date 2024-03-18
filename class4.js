e20237d2ba688a3971375ec1380f11b31b1de86b
tabiqchohan <160527853+tabiqchohan@users.noreply.github.com>
2024-03-18 03:01:48 -0700

    Add files via upload

---
+++ class4.js
@@ -0,0 +1,13 @@
+function halffryegg() {
+    var cooked = 1 + 1.5 + 2;
+    // console.log(cooked);
+    return cooked;
+}
+var egg = halffryegg();
+console.log(egg);
+Number;
+{
+    return egg + butter + salt;
+}
+var response = halffryegg(1, 1.5, 2);
+console.log(response);
---
+++ class4.ts
@@ -0,0 +1,12 @@
+function halffryegg():number{
+    let cooked:number = 1+ 1.5 +2;
+    // console.log(cooked);
+    return cooked
+}
+const egg = halffryegg()
+console.log(egg);
+// function halffryegg(egg: number, butter: number, salt: number); Number{
+// return egg + butter + salt;
+// }
+// let response: number = halffryegg(1, 1.5, 2)
+// console.log(response)
